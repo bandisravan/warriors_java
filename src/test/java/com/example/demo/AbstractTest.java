9fb60b8ba75e9f73f99cb10390535899cfab549a
bandisravan <sravankumar0000@gmail.com>
2019-04-09 12:45:49 +0530

    commiting

--- src/test/java/com/example/demo/AbstractTest.java
+++ src/test/java/com/example/demo/AbstractTest.java
@@ -12,11 +12,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.example.AbcBankingApplication;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ing.AbcBankingApplication;
 
 
 
